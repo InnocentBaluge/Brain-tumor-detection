02835924bc59617e89a68ed41bb789d194706004
InnocentBaluge <innocent.baluge@yahoo.com>
2022-08-09 14:15:45 +0300

    Add Login and register to the app

--- app.py
+++ app.py
@@ -7,10 +7,25 @@ import cv2
 from keras.models import load_model
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
+ 
 
+#DB imports
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from flask import redirect, url_for, session
 
 app = Flask(__name__)
 
+
+
+app.secret_key = 'my-key'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'brain-tumor'
+  
+mysql = MySQL(app)
 
 model =load_model('BrainTumor10Epochs.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
@@ -18,9 +33,9 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 
 def get_className(classNo):
 	if classNo==0:
-		return "No Brain Tumor"
+		return "The image uploaded does not have Brain Tumor"
 	elif classNo==1:
-		return "Yes Brain Tumor"
+		return "The image uploaded has Brain Tumor"
 
 
 def getResult(img):
@@ -33,7 +48,7 @@ def getResult(img):
     return result
 
 
-@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
@@ -51,6 +66,59 @@ def upload():
         result=get_className(value) 
         return result
     return None
+
+@app.route('/', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username or password !'
+    return render_template('login.html', msg = msg)
+  
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+  
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            return redirect(url_for('index'))
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
 
 
 if __name__ == '__main__':
