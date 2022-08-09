2178e2a2c44fdf541ca33546766ee50ae0eae85a
InnocentBaluge <innocent.baluge@yahoo.com>
2022-08-09 10:24:41 +0300

    Add model and Flask web App

---
+++ mainTest.py
@@ -0,0 +1,17 @@
+from json import load
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model = load_model('BrainTumor10Epochs.h5')
+image = cv2.imread('pred\pred9.jpg')
+img = Image.fromarray(image)
+
+img = img.resize((64,64))
+img = np.array(img)
+input_img= np.expand_dims(img, axis=0)
+
+result = np.round(model.predict(input_img) ).astype(int)
+
+print(result)
